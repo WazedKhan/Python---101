567c79623429713f777dad90ddd7872d72034e9b
Wazed Khan <51821594+WazedKhan@users.noreply.github.com>
2022-10-15 15:37:06 +0600

    3rd class

---
+++ Practice/flow.py
@@ -0,0 +1,44 @@
+
+
+
+li = []
+
+# [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] - > remove() value
+
+#  0 ,1, 2, 3 -> pop() index
+
+# 1 - 10 
+
+string = ['Jake', 'Snowy', 'Spike', 'Oggy', 'Apple']
+
+var = range(1,11)
+
+var = list(var)
+
+# for i in var:
+#     if i%2 == 0:
+#         li.append(i)
+#     else:
+#         pass
+
+# print(li)
+
+# print(var)
+# var.pop(4)
+
+while True:
+
+
+
+
+# {kye : values}
+
+# dic = {'x':['name', 20, 'jan-10'], 'y':20, 'z': 49}
+
+# dic['a'] = ['name', 38, 'birth']
+
+# print(dic['a'])
+# print('Enter Name: ')
+# name = input()
+
+# print('Hello '+name+' ')
