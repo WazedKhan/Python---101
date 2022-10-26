e89af7da5ecca0dc6cbeeb1d2b958fbde18fdd1c
Wazed Khan <51821594+WazedKhan@users.noreply.github.com>
2022-10-26 15:37:23 +0600

    Quiz-01

---
+++ Practice/PracticeDay_26_10.py
@@ -0,0 +1,42 @@
+# create a variable called color with an appropriate value on the line below
+# (Remember, strings in Python must be enclosed in 'single' or "double" quotes)
+
+
+"""
+Here is the table of available arithmetic operations:
+
+
+
+| Operator | Name           | Description                                    |
+|----------|----------------|------------------------------------------------|
+| a + b    | Addition       | Sum of a and b                                 |
+| a - b    | Subtraction    | Difference of a and b                          |
+| a * b    | Multiplication | Product of a and b                             |
+| a / b    | True division  | Quotient of a and b                            |
+| a // b   | Floor division | Quotient of a and b, removing fractional parts |
+| a % b    | Modulus        | Integer remainder after division of a by b     |
+| a ** b   | Exponentiation | a raised to the power of b                     |
+| -a       | Negation       | The negative of a                              | """
+
+
+# Create a variable called 'radius' equal to half the diameter
+
+# Create a variable called 'area', using the formula( pi x radius^2 ) for the area of a circle: pi times the radius squared
+
+# Swap value of a with b
+a = 10
+b = 20
+# hint 1: মনে করুন আপনার দুটি গ্লাস কোক এবং পেপসি ভর্তি আছে, আপনি যদি কোকের গ্লাসে পেপসি এবং পেপসির গ্লাসে কোক রাখতে চান তবে আপনি কী করতে পারেন ?
+#       : Think of yourself having two glasses full of Coke and Pepsi, What can you do if you want to put Pepsi in Coke's glass and Coke in Pepsi's glass?
+
+# hint 2: আপনি অন্য গ্লাস ব্যবহার করতে পারেন
+# hint 3: আপনি আরেকটি নতুন গ্লাসে পেপসি রাখতে পারেন তারপর পেপসির গ্লাসে কোক রাখুন তারপর পেপসিটিকে কোকের গ্লাসে রাখুন।
+
+
+# Given a list and target value find if the target value exists in the list if exists print the index number of the value,
+# if not print "the target doesn't exist".
+
+cards = [13, 11, 10, 7, 4, 3, 1, 0]
+query = 7
+output = 3
+
