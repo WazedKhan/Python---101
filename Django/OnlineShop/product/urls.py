404a792b7d957c3c21bc66a139a8a0c740488714
Wazed Khan <51821594+WazedKhan@users.noreply.github.com>
2022-11-23 23:03:13 +0600

    create/post method

--- Django/OnlineShop/product/urls.py
+++ Django/OnlineShop/product/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from product.views import index, create_category
 
 urlpatterns = [
-    path('category-list', index, name='category-list'),
-    path('category-create', create_category, name='category-create'),
-] 
+    path('category-list/', index, name='category-list'),
+    path('category-create/', create_category, name='category-create'),
+]
--- Django/OnlineShop/product/views.py
+++ Django/OnlineShop/product/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
+
 from product.models import Category
 
 # Create your views here.
@@ -9,4 +11,11 @@ def index(request):
     return render(request, 'index.html', context)
 
 def create_category(request):
+    if request.method == "POST":
+        Category.objects.create(
+            name=request.POST.get('category'),
+            user=request.user
+        )
+        return redirect('category-list')
+    
     return render(request, 'category-create.html')
