c0328efd89f995539ef42dd0919b899afd9835a4
WazedKhan <51821594+WazedKhan@users.noreply.github.com>
2022-12-01 14:16:02 +0600

    mysql connection

--- Django/OnlineShop/OnlineShop/settings.py
+++ Django/OnlineShop/OnlineShop/settings.py
@@ -82,8 +82,12 @@ WSGI_APPLICATION = 'OnlineShop.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'onlineshop',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
     }
 }
 
